45d03915413d065822c5c2e751fcf3247a910e69
T-800 <yavtushenko.aa@yandex.ru>
2023-02-26 17:57:46 +0300

    Yavtushenko HW 1

---
+++ SearchTestsNWYa.java
@@ -0,0 +1,11 @@
+public class SearchTestsNWYa {import org.junit.jupiter.api.Test;
+
+    public class SearchTests {
+        @Test
+        void successfulSearchTest() {
+            open("https://www.google.com/");
+            $("[name=q]").setValue("selenide").pressEnter();
+            $("[id=search]").shouldHave(text("https://selenide.org"));
+        }
+    }
+}
